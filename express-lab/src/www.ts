f9c5bb21591deed3a5beb3792ed82b92b53d920e
volodymyr-ratushnyi <driverwolf@gmail.com>
2025-12-12 01:34:36 +0200

    init commit

---
+++ express-lab/src/www.ts
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+import app from './app.ts';
+import http from 'http';
+import {type HttpError} from "http-errors";
+import {normalizePort} from './utils/server_utils.ts';
+import debugLib from 'debug';
+
+const debug = debugLib('fullstack-lab/express-lab:server');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+const onError = (error: HttpError) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      //fallthrough
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      //fallthrough
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr?.port;
+  debug('Listening on ' + bind);
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
